3ff21b12d468f5483a227120ad2a8aa47d851207
Samuel Pitoňák <sampittko@gmail.com>
2019-10-14 16:46:08 +0200

    lab 03 task 1 in progress

--- labs/03/src/main/java/cbsp/SimpleClickFilter.java
+++ labs/03/src/main/java/cbsp/SimpleClickFilter.java
@@ -13,8 +13,37 @@ public class SimpleClickFilter extends ClickFilterBase {
 	 */
 	@Override
 	protected void process(float[] samples) {
+		double sum = 0, mean, threshold, meanSamplesStd;
+		double[] samplesStd = new double[samples.length];
+
+		for (int i = 0; i < samples.length; i++) {
+			sum += Math.abs(samples[i]);
+		}
+
+		mean = sum / samples.length;
+
+		System.out.println(mean);
+
+		for (int i = 0; i < samples.length; i++) {
+			samplesStd[i] = Math.abs(samples[i] - mean);
+		}
+
+		sum = 0;
+
+		for (int i = 0; i < samplesStd.length; i++) {
+			sum += Math.abs(samplesStd[i]);
+		}
+
+		threshold = sum / samplesStd.length;
+
+		System.out.println(threshold);
 
 		// TODO Task 1.
-		
+
+		for (int i = 0; i < samples.length; i++) {
+			if (i > 0 && Math.abs(samples[i - 1] - samples[i]) > threshold) {
+				samples[i] = 0.0f;
+			}
+		}
 	}
 }
