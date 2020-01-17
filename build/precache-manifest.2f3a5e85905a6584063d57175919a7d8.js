2f8b00b29a9a7e9f7974a3724c4c9ffe343ae75c
Samuel Pitoňák <sampittko@gmail.com>
2020-01-17 12:03:17 +0100

    add buiold

---
+++ build/precache-manifest.2f3a5e85905a6584063d57175919a7d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "539ac5613c5c29580534024e8d19ef28",
+    "url": "/index.html"
+  },
+  {
+    "revision": "26b0bc833abba0f67410",
+    "url": "/static/css/main.a53fbf6f.chunk.css"
+  },
+  {
+    "revision": "c4c697724e65f1bcd915",
+    "url": "/static/js/2.796d8f0a.chunk.js"
+  },
+  {
+    "revision": "5a25cb5e25d64a19a6cea59c6134c4f4",
+    "url": "/static/js/2.796d8f0a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "26b0bc833abba0f67410",
+    "url": "/static/js/main.822c9812.chunk.js"
+  },
+  {
+    "revision": "ec2dd538b676e00164c5",
+    "url": "/static/js/runtime-main.514e3c9d.js"
+  }
+]);
